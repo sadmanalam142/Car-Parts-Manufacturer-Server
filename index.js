feb9abf7ea607f4af16bf4507a8b25293c24bc02
sadmanalam142 <sadmanalam142@gmail.com>
2022-05-24 03:34:55 +0600

    connected to mongodb

--- index.js
+++ index.js
@@ -1,15 +1,34 @@
 const express = require('express');
 const cors = require('cors');
+require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const port = process.env.PORT || 5000;
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.kk9hb.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        await client.connect();
+        const partCollection = client.db("car_parts_Manufacturer").collection("parts");
+    }
+    finally {
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+
+
 app.get('/', (req, res) => {
     res.send('Hello from car manufacturer')
 });
 
-app.listen(port, ()=> {
+app.listen(port, () => {
     console.log('Successfully listening from', port)
 });
