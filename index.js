98a9372a85dfb9240742a1b0e18b9aff188345b8
sadmanalam142 <sadmanalam142@gmail.com>
2022-05-26 03:11:48 +0600

    order done

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
 const app = express();
 
@@ -15,7 +15,40 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 async function run() {
     try {
         await client.connect();
-        const partCollection = client.db("car_parts_Manufacturer").collection("parts");
+        const partCollection = client.db("car_parts_manufacturer").collection("parts");
+        const orderCollection = client.db("car_parts_manufacturer").collection("orders");
+
+        app.get('/part', async (req, res) => {
+            const parts = await partCollection.find().toArray();
+            res.send(parts)
+        })
+
+        app.get('/part/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = {_id: ObjectId(id)};
+            const parts = await partCollection.findOne(query);
+            res.send(parts)
+        })
+
+        app.put('/part/:id', async (req, res) => {
+            const id = req.params.id;
+            const updatedQuantity = req.body;
+            const filter = { _id: ObjectId(id) };
+            const options = { upsert: true };
+            const updateDoc = {
+                $set: {
+                    availableQuantity: updatedQuantity.quantity
+                }
+            };
+            const parts = await partCollection.updateOne(filter, updateDoc, options);
+            res.send(parts)
+        })
+
+        app.post('/order', async (req, res) => {
+            const order = req.body;
+            const orders = await orderCollection.insertOne(order);
+            res.send(orders)
+        })
     }
     finally {
         // await client.close();
