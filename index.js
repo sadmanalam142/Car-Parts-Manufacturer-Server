12d7129d9220bf2b125636193c2317aeea0f273f
sadmanalam142 <sadmanalam142@gmail.com>
2022-10-19 09:10:04 +0600

    some changes

--- index.js
+++ index.js
@@ -27,7 +27,6 @@ function verifyJWT(req, res, next) {
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.kk9hb.mongodb.net/?retryWrites=true&w=majority`;
-console.log(uri);
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 async function run() {
